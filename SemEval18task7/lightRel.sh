6793ec6f81219c7ea4ffd2fd1f6b024f408078a3
gunjianpan <iofu728@163.com>
2019-03-30 23:30:54 +0800

    😶 change f1 score script

--- SemEval18task7/lightRel.sh
+++ SemEval18task7/lightRel.sh
@@ -29,19 +29,17 @@ if [[ "$k" == "0" ]]; then
     echo "---converting sentences to vectors---"
     python3 sent2vec.py ${k}
 
-    # cd ${libLinDir}
     echo "---training LibLinear model---"
     train -s ${s} -c ${c} -e ${e} ${modelDir}"libLinearInput_train.txt" ${modelDir}${out_name}".model"
     echo "---predicting on test set---"
     predict ${modelDir}"libLinearInput_test.txt" ${modelDir}${out_name}".model" ${modelDir}"predictions.txt"
 
-    cd ${mainDir}
     echo "---adding labels to LibLinear output---"
     python3 addLabels.py ${k}
 
-    perl "semeval2018_task7_scorer-v1.2.pl" ${modelDir}"predictions_with_labels.txt" ${modelDir}answer_key.txt
-
-elif [[ "$k" > "0" ]]; then
+elif
+    [[ "$k" > "0" ]]
+then
     echo "---running ${k}-fold cross-validation---"
     rm -r ${resultsDir}
     mkdir ${resultsDir}
@@ -64,9 +62,22 @@ elif [[ "$k" > "0" ]]; then
         echo "---adding labels to LibLinear output---"
         python3 addLabels.py ${i}
 
-        echo "---writing results to file---"
-        perl "semeval2018_task7_scorer-v1.2.pl" ${modelDir}"predictions_with_labels.txt" ${modelDir}answer_key.txt >${resultsDir}"results${i}.txt"
     done
-
+    m=0
     python3 average.py
+
+    echo "---competition run---"
+    echo "---running feature extraction---"
+    python3 featureExtraction.py ${m}
+
+    echo "---converting sentences to vectors---"
+    python3 sent2vec.py ${m}
+
+    echo "---training LibLinear model---"
+    train -s ${s} -c ${c} -e ${e} ${modelDir}"libLinearInput_train.txt" ${modelDir}${out_name}".model"
+    echo "---predicting on test set---"
+    predict ${modelDir}"libLinearInput_test.txt" ${modelDir}${out_name}".model" ${modelDir}"predictions.txt"
+
+    echo "---adding labels to LibLinear output---"
+    python3 addLabels.py ${m}
 fi
