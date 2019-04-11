aa385b4e05522ce24a33c3803d677db06012f712
gunjianpan <iofu728@163.com>
2019-04-11 21:23:50 +0800

    🐑update micro & macro f1

--- SemEval18task7/util.py
+++ SemEval18task7/util.py
@@ -2,7 +2,7 @@
 @Author: gunjianpan
 @Date:   2019-03-29 19:05:54
 @Last Modified by:   gunjianpan
-@Last Modified time: 2019-03-30 22:37:48
+@Last Modified time: 2019-04-10 20:32:46
 '''
 import os
 import pickle
@@ -36,40 +36,43 @@ def load_bigger(input_file):
 
 
 @jit
-def fastF1(result, predict, trueValue):
-    """
-    f1 score
-    """
-    trueNum = 0
-    recallNum = 0
-    precisionNum = 0
-    for index, values in enumerate(result):
-        if values == trueValue:
-            recallNum += 1
-            if values == predict[index]:
-                trueNum += 1
-        if predict[index] == trueValue:
-            precisionNum += 1
-    R = trueNum / recallNum if recallNum else 0
-    P = trueNum / precisionNum if precisionNum else 0
-    f1 = (2 * P * R) / (P + R) if (P + R) else 0
-    # print(id2rela[trueValue], P, R, f1)
-    return P, R, f1
+def fastF1(result, predict):
+    ''' f1 score '''
+    true_total, r_total, p_total, p, r = 0, 0, 0, 0, 0
+    total_list = []
+    for trueValue in range(6):
+        trueNum, recallNum, precisionNum = 0, 0, 0
+        for index, values in enumerate(result):
+            if values == trueValue:
+                recallNum += 1
+                if values == predict[index]:
+                    trueNum += 1
+            if predict[index] == trueValue:
+                precisionNum += 1
+        R = trueNum / recallNum if recallNum else 0
+        P = trueNum / precisionNum if precisionNum else 0
+        true_total += trueNum
+        r_total += recallNum
+        p_total += precisionNum
+        p += P
+        r += R
+        f1 = (2 * P * R) / (P + R) if (P + R) else 0
+        print(id2rela[trueValue], P, R, f1)
+        total_list.append([P, R, f1])
+    p /= 6
+    r /= 6
+    micro_r = true_total / r_total
+    micro_p = true_total / p_total
+    macro_f1 = (2 * p * r) / (p + r) if (p + r) else 0
+    micro_f1 = (2 * micro_p * micro_r) / (micro_p +
+                                          micro_r) if (micro_p + micro_r) else 0
+    print('P: {:.2f}%, R: {:.2f}%, Micro_f1: {:.2f}%, Macro_f1: {:.2f}%'.format(
+        p*100, r*100, micro_f1 * 100, macro_f1*100))
+    return p, r, macro_f1, total_list
 
 
 def scoreSelf(predict, result=None):
     if result is None:
-        with open('%skeys.test.%s.txt' % (data_dir, task_number), 'r') as f:
+        with open('{}keys.test.{}.txt'.format(data_dir, task_number), 'r') as f:
             result = [rela2id[ii.split('(')[0]] for ii in f.readlines()]
-    p, r = 0, 0
-    result_list = []
-    for ii in range(6):
-        tp, tr, tf1 = fastF1(result, predict, ii)
-        result_list.append([tp, tr, tf1])
-        p += tp
-        r += tr
-    p /= 6
-    r /= 6
-    f1 = (2 * p * r) / (p + r) if (p + r) else 0
-    # print('Once', p, r, f1)
-    return p, r, f1, result_list
+    return fastF1(result, predict)
